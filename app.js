d59c4c42624614eb4741ed9bfd1177d5ea9bdf79
rajmeetsbal <singh.rajmeet.st@gmail.com>
2020-02-27 10:37:43 +0530

    changed hello msg

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello Universe!'))
+app.get('/', (req, res) => res.send('Hello Universe2'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
