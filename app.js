a28fae8278d9760a3b4b1c238ef5db475e817f86
Rajmeet Bal <rajmeetbal@in.ibm.com>
2020-02-28 12:10:01 +0530

    added mongo to app

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+const mongoose = require('mongoose');
 const express = require('express')
 const app = express()
 const port = 8080
+
+mongoose.connect(mongodb://172.21.154.130:27017/testdb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection
+  .on('open', () => {
+    console.log('Mongoose connection open');
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
 
 app.get('/', (req, res) => res.send('Hello Universe2'))
 
