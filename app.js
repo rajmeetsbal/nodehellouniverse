73e6e576918db5ad2ca1f6e07fb99dba70ff7ae6
rajmeetsbal <singh.rajmeet.st@gmail.com>
2020-02-28 15:44:04 +0530

    added show

--- app.js
+++ app.js
@@ -43,11 +43,49 @@ db.once('open', function() {
 
 
 
+app.post("/add", function (req, res) {
+	var n = Math.random();
+	var BookSchema = mongoose.Schema({
+      name: String,
+      price: Number,
+      quantity: Number
+    });
+ 
+    // compile schema to model
+    var Book = mongoose.model('Book', BookSchema, 'bookstore');
+ 
+    // a document instance
+    var book1 = new Book({ name: 'Introduction to Mongoose'+n, price: 10+n, quantity: 25+n });
+ 
+    // save model to database
+    book1.save(function (err, book) {
+      if (err) return console.error(err);
+      console.log(book.name + " saved to bookstore collection.");
+    });
+});
+
+app.get("/show", function (req, res) {
+	var BookSchema = mongoose.Schema({
+      name: String,
+      price: Number,
+      quantity: Number
+    });
+ 
+    // compile schema to model
+    var Book = mongoose.model('Book', BookSchema, 'bookstore');
+ 
+	Book.find({}, function(error, books) {
+		console.log(books); //Display the comments returned by MongoDB, if any were found. Executes after the query is complete.
+		res.send(books);
+	});
+});
 
 
 
+app.get('/', (req, res) => res.send('Hello Universe1'));
+app.get('/two', (req, res) => res.send('Hello Universe2'));
 
-app.get('/', (req, res) => res.send('Hello Universe1'))
-app.get('/two', (req, res) => res.send('Hello Universe2'))
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
