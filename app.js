9e0e467802f851bd3c32925a93b54e84055c51fc
rajmeetsbal <singh.rajmeet.st@gmail.com>
2020-02-28 12:50:50 +0530

    add doc

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 8080
 
 mongoose.connect('mongodb://172.21.154.130:27017/testdb', {useNewUrlParser: true,useUnifiedTopology: true});
 
-mongoose.connection
+var db = mongoose.connection
   .on('open', () => {
     console.log('Mongoose connection open');
   })
@@ -14,7 +14,37 @@ mongoose.connection
   });
 
 console.log("Connected to Mongo");
-console.log("Connected to Mongo"+mongoose);
+console.log("Connected to Mongo"+db);
+
+
+db.once('open', function() {
+    console.log("Connection Successful!");
+    
+    // define Schema
+    var BookSchema = mongoose.Schema({
+      name: String,
+      price: Number,
+      quantity: Number
+    });
+ 
+    // compile schema to model
+    var Book = mongoose.model('Book', BookSchema, 'bookstore');
+ 
+    // a document instance
+    var book1 = new Book({ name: 'Introduction to Mongoose', price: 10, quantity: 25 });
+ 
+    // save model to database
+    book1.save(function (err, book) {
+      if (err) return console.error(err);
+      console.log(book.name + " saved to bookstore collection.");
+    });
+    
+}); 
+
+
+
+
+
 
 
 app.get('/', (req, res) => res.send('Hello Universe1'))
